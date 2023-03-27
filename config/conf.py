4f13397865b6e98c6d07782eac0e23302072947a
Fedor <shmakov987@mail.ru>
2023-03-28 00:19:16 +0300

    hz

---
+++ config/conf.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# kinobot documentation build configuration file, created by
+# sphinx-quickstart on Sat Mar 27 15:14:35 2023.
+#
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../dist'))
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'kinobot'
+author = 'Fedor Shmakov, Nikolay Khorov, Ivan Demin'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'alabaster'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_static_path = ['_static']
